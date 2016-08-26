7fc4ef1f8950c39bed5ec19513966cebe70dd577
Shailendra Chauhan <pro.shailendra@gmail.com>
2016-08-26 20:03:49 +0530

    added code
    added code

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/angular-ui-router/index.d.ts" />
+/// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/body-parser/index.d.ts" />
+/// <reference path="globals/express/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
